2a81f63817f9ee663cc9db8adb2919b1682816b5
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-07-31 18:56:55 +0530

    added soltuion for the problem

---
+++ Trapping rain water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& h) 
+    {
+        int n = h.size();
+        if(n == 0)
+        {
+            return 0;
+        }
+        vector<int>left(n);
+        vector<int>right(n);
+        left[0] = h[0];
+        for(int i = 1; i < n; i++)
+        {
+            left[i] = max(left[i - 1], h[i]);
+        }
+        right[n - 1] = h[n - 1];
+        for(int i = n - 2; i >= 0; i--)
+        {
+            right[i] = max(right[i + 1], h[i]);
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            ans += (min(left[i], right[i]) - h[i]);
+        }
+        return ans;
+    }
+};
